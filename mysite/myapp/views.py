2057e11807e36aa14202df834414e52cacd6f02e
Mxolisi Hlakanyana <mxolisihlakanyana90@gmail.com>
2024-12-05 04:56:33 +0200

    first commit

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home_page(request):
-    return render(request, 'base.html')
+
+def index(request):
+  if request.method == 'port':
+    city = request.POST['city']
+    source = urllib.request.urlopen('http://api-openweathermap.org/data/2.5/weather?q=' + 
+    city + '&appid=30258322651f8b903747f465be27dc31').read() 
+
+
+    list_of_data = json.loads(source)
+
+data = {
+         "country_code": str(list_of_data['sys']['country']),
+}
+
